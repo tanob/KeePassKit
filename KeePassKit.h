60a1263a6045d67e2d71ee2cec2dd6f56945d1e3
Adriano Bonat <adrianob@gmail.com>
2015-02-08 13:14:36 -0200

    Adding missing imports.

--- KeePassKit.h
+++ KeePassKit.h
@@ -6,6 +6,9 @@
 //  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
 //
 
+#import "KPKTree.h"
+#import "KPKTree+Serializing.h"
+#import "KPKCompositeKey.h"
 #import "KPKNode.h"
 #import "KPKEntry.h"
 #import "KPKGroup.h"
